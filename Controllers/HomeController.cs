57d88278325ba9688ba167e1ac6a0549aedbae9c
GoddM <GoddM@MG-PROFF>
2023-05-19 22:46:14 +0100

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Benchmark.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Transactions;
+using Benchmark.Services;
+using Benchmark.Services.Interfaces;
+
+namespace Benchmark.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IStringCalculationService _stringCalculationService;
+        public HomeController(IStringCalculationService stringCalculationService)
+        {
+            _stringCalculationService = stringCalculationService;
+        }
+
+        public IActionResult Index()
+        {
+            StringCalculatorViewModel viewModel = new StringCalculatorViewModel { Output = null};
+            return View(viewModel);
+        }
+        [HttpPost]
+        public IActionResult Index(StringCalculatorViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(viewModel);
+            }
+
+            viewModel.Output = _stringCalculationService.Add(viewModel.Input);
+
+            return View(viewModel);
+        }
+        
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <returns></returns>
+       
+
+       
+    }
+}
---
+++ Models/StringCalculatorViewModel.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Benchmark.Models
+{
+    public class StringCalculatorViewModel
+    {
+        public string Input { get; set; }
+
+        public int? Output { get; set; }
+    }
+}
